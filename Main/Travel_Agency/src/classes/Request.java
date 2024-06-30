7aa9fabfee1212908d08f5ad968d79b01b1144c4
Rodcvjoao <rcjoao@id.uff.br>
2024-06-30 20:16:30 -0300

    UpdateRequest

--- Main/Travel_Agency/src/classes/Request.java
+++ Main/Travel_Agency/src/classes/Request.java
@@ -1,18 +1,38 @@
 package classes;
 
 public class Request{
+    private String Name;
+    private int MaxPrice;
     private String DepartureLocation;
     private String Destination;
     private int NightAmount;
     private int Stars;
     private Boolean Accepted;
 
-    public Request(int NightAmount, String DepartureLocation, String Destination, int Stars){
+    public Request(String Name, int MaxPrice, int NightAmount, String DepartureLocation, String Destination, int Stars){
         this.DepartureLocation = DepartureLocation;
         this.Destination = Destination;
         this.Stars = Stars;
         this.NightAmount = NightAmount;
+        this.Name = Name;
+        this.MaxPrice = MaxPrice;
         this.Accepted = false;
+    }
+
+    public String getName(){
+        return Name;
+    }
+      
+    public void setName(String Name){
+    this.Name = Name;
+    }
+    
+    public int getMaxPrice(){
+    return MaxPrice;
+    }
+
+    public void setMaxPrice(int MaxPrice){
+    this.MaxPrice = MaxPrice;
     }
 
     public String getDepartureLocation(){
--- Main/Travel_Agency/src/classes/ReservaService.java
+++ Main/Travel_Agency/src/classes/ReservaService.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 public class ReservaService {
 
-    public static Hotel encontraMelhorHotel(List<Hotel> hoteisDisponiveis, String destino, int estrelasMinimas, int precoMax, int reserva){ // recebe a lista de hoteis disponíveis no momento, a cidade
+    public static Hotel encontraMelhorHotel(List<Hotel> hoteisDisponiveis, String destino, int estrelasMinimas, int precoMax){ // recebe a lista de hoteis disponíveis no momento, a cidade
         // desejada, a quantidade mínima de estrelas e o preço máximo exigidos pelo cliente.
         int menorPreco = precoMax; //inicializamos o menor preço com o preço máximo que o cliente esta disposto a pagar
         Hotel melhorHotel = null; // inicializamos o melhor hotel como nulo 
         for(Hotel hotel:hoteisDisponiveis){ // para cada hotel disponível
-            if(hotel.getHotelLocation().equals(destino) && hotel.getVacancyAmount() <= reserva && hotel.getHotelStars() >= estrelasMinimas && hotel.getNightCost() < menorPreco){ // se o hotel fica na cidade desejada, tem pelo menos o
+            if(hotel.getHotelLocation().equals(destino) && hotel.getVacancyAmount() >= 1 && hotel.getHotelStars() >= estrelasMinimas && hotel.getNightCost() < menorPreco){ // se o hotel fica na cidade desejada, tem pelo menos o
                 // mínimo de estrelas exigido e tem um preço menor que o menor preço até agora 
                 menorPreco = hotel.getNightCost(); // o menor preço passa a ser o desse hotel
                 melhorHotel = hotel; // esse hotel passa a ser o melhor hotel para esse cliente
@@ -18,7 +18,7 @@ public class ReservaService {
         return melhorHotel; // por fim, retornamos o melhor hotel para o cliente no momento em que foi feita a requisição
     }
 
-    public static List<Flight> encontrarMelhorVoo(List<Flight> voosDisponiveis, String origem, String destino, int precoMax, int assentos) { // recebe a lista de voos disponíveis, a cidade de saída e a
+    public static List<Flight> encontrarMelhorVoo(List<Flight> voosDisponiveis, String origem, String destino, int precoMax) { // recebe a lista de voos disponíveis, a cidade de saída e a
         // cidade de destino do cliente
         Map<String, List<Flight>> mapaVoos = new HashMap<>(); // criamos um mapa com a origem do voo como a chave e uma lista de voos com essa origem como info
         for (Flight voo : voosDisponiveis) {
@@ -30,18 +30,18 @@ public class ReservaService {
 
         for (Flight voo1 : mapaVoos.getOrDefault(origem, Collections.emptyList())) { // para cada voo com a origem do cliente no mapa de voos 
             // (se não existir, o valor padrão retornado é uma lista vazia, ou seja, não existe voo com essa origem)
-            if (voo1.getDestination().equals(destino) && voo1.getEmptySeats() <= assentos && voo1.getFlightPrice() < melhorPreco) { // se o destino desse voo é o destino do cliente e o preço do voo é o menor até agora
+            if (voo1.getDestination().equals(destino) && voo1.getEmptySeats() >= 1 && voo1.getFlightPrice() < melhorPreco) { // se o destino desse voo é o destino do cliente e o preço do voo é o menor até agora
                 resultado = Arrays.asList(voo1); // esse passa a ser o melhor voo para o cliente
                 melhorPreco = voo1.getFlightPrice(); // o menor preço passa a ser o desse voo
             } else { // senão,
                 for (Flight voo2 : mapaVoos.getOrDefault(voo1.getDestination(), Collections.emptyList())) { // para cada voo com a origem sendo o destino do voo 1
-                    if (voo2.getDestination().equals(destino) && voo1.getEmptySeats() <= assentos && voo2.getEmptySeats() <= assentos && (voo1.getFlightPrice() + voo2.getFlightPrice()) < melhorPreco) { // se o destino do voo 2 é o destino do cliente e os preços do voo 1 e do voo 2
+                    if (voo2.getDestination().equals(destino) && voo1.getEmptySeats() >= 1 && voo2.getEmptySeats() >= 1 && (voo1.getFlightPrice() + voo2.getFlightPrice()) < melhorPreco) { // se o destino do voo 2 é o destino do cliente e os preços do voo 1 e do voo 2
                         // somados for menor que o melhor preço até o momento
                         resultado = Arrays.asList(voo1, voo2); // esses passam a ser os melhores voos para o cliente
                         melhorPreco = voo1.getFlightPrice() + voo2.getFlightPrice(); // o melhor preço passa a ser o desses dois voos somados
                     } else { // senão,
                         for (Flight voo3 : mapaVoos.getOrDefault(voo2.getDestination(), Collections.emptyList())) { // para cada voo com a origem sendo o destino do voo 2
-                            if (voo3.getDestination().equals(destino) && voo1.getEmptySeats() <= assentos && voo2.getEmptySeats() <= assentos && voo3.getEmptySeats() <= assentos &&(voo1.getFlightPrice() + voo2.getFlightPrice() + voo3.getFlightPrice()) < melhorPreco) { // se o destino do voo 3 é o destino do cliente e os preços
+                            if (voo3.getDestination().equals(destino) && voo1.getEmptySeats() >= 1 && voo2.getEmptySeats() >= 1 && voo3.getEmptySeats() >= 1 &&(voo1.getFlightPrice() + voo2.getFlightPrice() + voo3.getFlightPrice()) < melhorPreco) { // se o destino do voo 3 é o destino do cliente e os preços
                                 // do voo 1, do voo 2 e do voo 3 somados for menor que o melhor preço até o momento
                                 resultado = Arrays.asList(voo1, voo2, voo3); // esses passam a ser os melhores voos para o cliente
                                 melhorPreco = voo1.getFlightPrice() + voo2.getFlightPrice() + voo3.getFlightPrice(); // o melhor preço passa a ser o desses três voos somados
